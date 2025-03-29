18f7581eba17c4806df912784bab93d8950693b6
css345 <padmajagajjarapu3@gmail.com>
2025-03-29 06:33:39 +0530

    Add files via upload

---
+++ LarComSubSeq.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class LarComSubSeq
+
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter string s1 : ");
+		String s1 = sc.next();
+		System.out.print("Enter string s2 : ");
+		String s2 = sc.next();
+		int res = larComSubSeq(s1,s2);
+		System.out.println("Largest Common subsequence length = "+res);
+	}
+	public static int larComSubSeq(String s1,String s2)
+	{
+		int m = s1.length();
+		int n = s2.length();
+		int[][] table = new int[m+1][n+1];
+		for(int i=0;i<=m;i++)
+		{
+			for(int j=0;j<=n;j++)
+			{
+				if(i==0 || j==0)
+					table[i][j] = 0;
+			}
+		}
+		for(int i=1;i<=m;i++)
+		{
+			for(int j=1;j<=n;j++)
+			{
+				if(s1.charAt(i-1) == s2.charAt(j-1))
+					table[i][j] = table[i-1][j-1]+1;
+				else
+					table[i][j] = Math.max(table[i-1][j],table[i][j-1]);
+			}
+		}
+		return table[m][n];
+	}
+}
